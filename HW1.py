d470ad9f5bf6314e13c415337f2a510f2314fb64
Charles Campbell <crc00042@mix.wvu.edu>
2024-08-30 10:48:43 -0400

    improved print statements and started CU function

--- HW1.py
+++ HW1.py
@@ -10,11 +10,11 @@ temps = np.array([98.51, 98.50, 98.50, 98.49, 98.52, 98.49, 98.52, 98.45, 98.47]
 times = [datetime.time(14, x, 16) for x in np.arange(45, 54, 1)]
 minutesElapsed = np.arange(0, len(temps), 1)
 # Arithmetic mean of the temperature measurements.
-meanTemp = np.mean(temps)
-print(meanTemp)
+meanTemp = round(np.mean(temps), 5)
+print("Mean temperature is ", meanTemp)
 # Standard deviation of the temperature measurements.
 tempsStDev = round(np.std(temps), 5)
-print(tempsStDev)
+print("Standard deviation of temperature measurements is", tempsStDev)
 # Individual temperature measurement uncertanties for cases A, B, and C.
 ATempUncertainty = 0.000
 BTempUncertainty = 0.01
@@ -26,11 +26,12 @@ def getOverallUnvertanty(caseTempUncertainty, stDev):
 ovUncertA = getOverallUnvertanty(ATempUncertainty, tempsStDev)
 ovUncertB = getOverallUnvertanty(BTempUncertainty, tempsStDev)
 ovUncertC = getOverallUnvertanty(CTempUncertainty, tempsStDev)
-print(ovUncertA, ovUncertB, ovUncertC)
+print("The overall uncertainty for cases A, B, and C are", ovUncertA, ovUncertB, ovUncertC)
 # Create a line of best fit of the temperature measurements over time
 slope, intercept, r_value, p_value, std_err = stats.linregress(minutesElapsed, temps)
 lineOfBestFit = np.poly1d([slope, intercept])
-print(slope)
+print("The slope of the line of best fit is", round(slope, 5))
+print("The y-intercept of the line of best fit is", round(intercept, 5))
 # Plot temperature measurements over minutes elapsed
 plt.scatter(minutesElapsed, temps, color="Blue", label="Temperature Measurements")
 plt.plot(minutesElapsed, lineOfBestFit(minutesElapsed), color="Red", label="Line of Best Fit")
@@ -38,3 +39,11 @@ plt.title("Water temperature measurements over time")
 plt.xlabel("Elapsed time (minutes)")
 plt.ylabel("Water temperature (degree Celsius)")
 plt.show()
+# Calculate common uncertainty
+def getCommonUncertainty (array, slope, intercept): 
+    commonUncertainty = np.sqrt(\
+        (1 / (len(array) - 2)) * np.sum(\
+            temps[x] - slope * minutesElapsed[x] - intercept) ** 2 for x in  np.arange(1, len(temps)))
+    return commonUncertainty
+commonUncertainty = getCommonUncertainty(temps, slope, intercept)
+print("The common uncertainty of the temperature measurements is", commonUncertainty)
